11c2268c627d5c7cfb5069097eb9c0b20f77ea86
glkt <vedrenne.benjamin@gmail.com>
2018-02-24 17:19:45 +0100

    fini models 3d + rangé projet + uv + script pour anim de textures etc.

---
+++ Assets/Script/shiftTexture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shiftTexture : MonoBehaviour {
+    public float speed = 4f;
+    public float continuousSpeed = 0.1f;
+    public float movement = 4f;
+    private Renderer rend;
+    private float timer = 1;
+    private Vector2 UVoffset;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+    void Update() {
+    	timer -= Time.deltaTime;
+    	UVoffset.y -= Time.deltaTime * continuousSpeed;
+    	if(timer<0){
+    		timer = Random.Range(0,speed);
+    		UVoffset.x += Random.value/movement;
+    		UVoffset.y += Random.value/movement;
+    		
+    	}
+    	rend.material.SetTextureOffset("_MainTex", UVoffset);
+    }
+}
